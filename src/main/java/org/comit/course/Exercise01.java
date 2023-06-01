fb664f38abee65c493d491d88a290bb72151d47e
Rajeev Kotha <kotha.rajeev@gmail.com>
2023-05-31 19:17:10 -0600

    First exercise

---
+++ src/main/java/org/comit/course/Exercise01.java
@@ -0,0 +1,12 @@
+package org.comit.course;
+
+public class Exercise01 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		System.out.print("Hi, I already print sentences.");
+		
+	}
+
+}
